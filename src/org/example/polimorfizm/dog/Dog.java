5aef8ebf7a92c884932598ad7c501e8798b11c05
winnermann <zotov.k1983@mail.ru>
2020-08-24 00:04:09 +0300

    Добавлены комменарии  в классы Dog, Cat4

--- src/org/example/polimorfizm/dog/Dog.java
+++ src/org/example/polimorfizm/dog/Dog.java
@@ -4,7 +4,7 @@ import org.example.polimorfizm.animal.Animal;
 
 public class Dog extends Animal {
 
-    //В public class Dog нет методов, методы наследуются из public class Animal
+    //В public class Dog нет методов, методы наследуются из родительского класса public class Animal
     //из public class Animal наследуется метод public void eat()
 
 }
